62ab0b1c932951f4fbcc4f4626bc18e2f57fd6a0
Ashutosh74554 <ashutoshdash1437@gmail.com>
2024-04-17 14:49:58 +0530

    Turned design into basic frontend template

--- blockchain-voting-app/src/App.js
+++ blockchain-voting-app/src/App.js
@@ -1,12 +1,76 @@
-import './App.css';
-import LoginForm from './Components/LoginForm/LoginForm';
+import { useEffect } from "react";
+import {
+  Routes,
+  Route,
+  useNavigationType,
+  useLocation,
+} from "react-router-dom";
+import SignIn from "./pages/SignIn/SignIn";
+import UserHome from "./pages/UserHome/UserHome";
+import Vote from "./pages/Vote/Vote";
+import AdminHome from "./pages/AdminHome/AdminHome";
+import Candidates from "./pages/Candidates/Candidates";
 
 function App() {
+  const action = useNavigationType();
+  const location = useLocation();
+  const pathname = location.pathname;
+
+  useEffect(() => {
+    if (action !== "POP") {
+      window.scrollTo(0, 0);
+    }
+  }, [action, pathname]);
+
+  useEffect(() => {
+    let title = "";
+    let metaDescription = "";
+
+    switch (pathname) {
+      case "/":
+        title = "Blockchain Based Voting System";
+        metaDescription = "";
+        break;
+      case "/sign-in":
+        title = "Sign In";
+        metaDescription = "";
+        break;
+      case "/vote":
+        title = "Vote";
+        metaDescription = "";
+        break;
+      case "/admin-home":
+        title = "Blockchain Based Voting System";
+        metaDescription = "";
+        break;
+      case "/candidates":
+        title = "Candidates";
+        metaDescription = "";
+        break;
+    }
+
+    if (title) {
+      document.title = title;
+    }
+
+    if (metaDescription) {
+      const metaDescriptionTag = document.querySelector(
+        'head > meta[name="description"]'
+      );
+      if (metaDescriptionTag) {
+        metaDescriptionTag.content = metaDescription;
+      }
+    }
+  }, [pathname]);
+
   return (
-    <div>
-      <LoginForm />
-    </div>
+    <Routes>
+      <Route path="/" element={<UserHome />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/vote" element={<Vote />} />
+      <Route path="/admin-home" element={<AdminHome />} />
+      <Route path="/candidates" element={<Candidates />} />
+    </Routes>
   );
 }
-
 export default App;
